b9500366b636afdfa5ec3d56db7ee3741f9e2be6
Thiago Soares <thiagocarv4@gmail.com>
2025-05-05 17:49:09 -0300

    implementando paginação

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,9 +1,94 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
 import { z } from "zod";
+import { AppError } from "@/utils/app-error";
+
+const CategoriesEnum = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accommodation",
+]);
 
 class RefundsController {
   async create(request: Request, response: Response) {
-    response.status(201).json({ message: "ok" });
+    const bodySchema = z.object({
+      name: z
+        .string()
+        .trim()
+        .min(1, { message: "Informe o nome da solicitação" }),
+      category: CategoriesEnum,
+      amount: z.number().positive({ message: "O valor precisa ser positivo" }),
+      filename: z.string().min(20),
+    });
+
+    const { name, category, amount, filename } = bodySchema.parse(request.body);
+
+    if (!request.user.id) {
+      throw new AppError("Unauthorized", 401);
+    }
+
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        category,
+        amount,
+        filename,
+        userId: request.user?.id,
+      },
+    });
+
+    response.status(201).json(refund);
+  }
+
+  async index(request: Request, response: Response) {
+    const querySchema = z.object({
+      name: z.string().optional().default(""),
+      page: z.coerce.number().optional().default(1),
+      perPage: z.coerce.number().optional().default(10),
+    });
+
+    const { name, page, perPage } = querySchema.parse(request.query);
+
+    const skip = (page - 1) * perPage;
+
+    const refunds = await prisma.refunds.findMany({
+      skip,
+      take: perPage,
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          },
+        },
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    const totalRecords = await prisma.refunds.count({
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          },
+        },
+      },
+    });
+
+    const totalPages = Math.ceil(totalRecords / perPage);
+
+    response.json({
+      refunds,
+      pagination: {
+        page,
+        perPage,
+        totalRecords,
+        totalPages: totalPages > 0 ? totalPages : 1,
+      },
+    });
   }
 }
 
--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -6,4 +6,5 @@ const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 
+
 export { usersRoutes };
