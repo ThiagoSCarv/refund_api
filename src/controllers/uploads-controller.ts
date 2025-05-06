04f4da38e4a4dc26c1184844ce70e4fa6f80f9e1
Thiago Soares <thiagocarv4@gmail.com>
2025-05-06 17:03:47 -0300

    Manipulando arquivo

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import { z } from "zod";
 import uploadConfig from "@/configs/upload";
+import { DiskStorage } from "@/providers/disk-storage";
+import { ZodError } from "zod";
 import { AppError } from "@/utils/app-error";
 
 class UploadsController {
   async create(request: Request, response: Response) {
+    const diskStorage = new DiskStorage();
+
     try {
       const fileSchema = z
         .object({
@@ -27,10 +31,19 @@ class UploadsController {
         })
         .passthrough();
 
-      const { file } = fileSchema.parse(request.file);
+      const file = fileSchema.parse(request.file);
+      const filename = await diskStorage.saveFile(file.filename);
 
-      response.json({ message: "ok" });
+      response.json({ filename });
     } catch (error) {
+      if (error instanceof ZodError) {
+        if (request.file) {
+          await diskStorage.deleteFile(request.file.filename, "tmp");
+        }
+
+        throw new AppError(error.issues[0].message);
+      }
+      
       throw error;
     }
   }
